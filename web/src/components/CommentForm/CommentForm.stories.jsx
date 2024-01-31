2c001413bb4cd9123056199d314eb4253abdf37f
TaherMAfini <tma29@njit.edu>
2024-01-31 15:36:28 -0500

    Chapter 6

---
+++ web/src/components/CommentForm/CommentForm.stories.jsx
@@ -0,0 +1,21 @@
+import CommentForm from './CommentForm'
+
+export default { component: CommentForm }
+
+export const Primary = () => {
+  mockGraphQLMutation('CreateCommentMutation', (variables, { ctx }) => {
+    const id = Math.floor(Math.random() * 1000)
+    ctx.delay(1000)
+
+    return {
+      createComment: {
+        id,
+        name: variables.input.name,
+        body: variables.input.body,
+        createdAt: new Date().toISOString(),
+      },
+    }
+  })
+
+  return <CommentForm />
+}
