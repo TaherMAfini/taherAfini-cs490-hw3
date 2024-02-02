b714449af74c3814a1a468a7e2d1ea43aae97eec
TaherMAfini <tma29@njit.edu>
2024-02-01 20:04:03 -0500

    Chapter 7

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,4 +1,6 @@
-import { comments } from './comments'
+import { AuthenticationError, ForbiddenError } from '@redwoodjs/graphql-server'
+
+import { comments, createComment, deleteComment } from './comments'
 import { db } from 'src/lib/db'
 
 describe('comments', () => {
@@ -28,4 +30,42 @@ describe('comments', () => {
     expect(comment.postId).toEqual(scenario.post.bark.id)
     expect(comment.createdAt).not.toEqual(null)
   })
+
+  scenario('allows a moderator to delete a comment', async (scenario) => {
+    mockCurrentUser({ roles: ['moderator'] })
+
+    const comment = await deleteComment({
+      id: scenario.comment.jane.id,
+    })
+    expect(comment.id).toEqual(scenario.comment.jane.id)
+
+    const result = await comments({ postId: scenario.comment.jane.postId })
+    expect(result.length).toEqual(0)
+  })
+
+  scenario(
+    'does not allow a non-moderator to delete a comment',
+    async (scenario) => {
+      mockCurrentUser({ roles: 'user' })
+
+      expect(() =>
+        deleteComment({
+          id: scenario.comment.jane.id,
+        })
+      ).toThrow(ForbiddenError)
+    }
+  )
+
+  scenario(
+    'does not allow a logged out user to delete a comment',
+    async (scenario) => {
+      mockCurrentUser(null)
+
+      expect(() =>
+        deleteComment({
+          id: scenario.comment.jane.id,
+        })
+      ).toThrow(AuthenticationError)
+    }
+  )
 })
--- web/src/components/Comment/Comment.stories.jsx
+++ web/src/components/Comment/Comment.stories.jsx
@@ -1,26 +1,35 @@
-// Pass props to your component by passing an `args` object to your story
-//
-// ```jsx
-// export const Primary = {
-//  args: {
-//    propName: propValue
-//  }
-// }
-// ```
-//
-// See https://storybook.js.org/docs/react/writing-stories/args.
-
 import Comment from './Comment'
 
 export default { component: Comment }
 
-export const Primary = () => {
+export const defaultView = () => {
   return (
     <Comment
       comment={{
+        id: 1,
         name: 'Rob Cameron',
         body: 'This is the first comment!',
-        createdAt: '2020-01-01T12:34:56Z'
+        createdAt: '2020-01-01T12:34:56Z',
+        postId: 1
+      }}
+    />
+  )
+}
+
+export const moderatorView = () => {
+  mockCurrentUser({
+    id: 1,
+    email: 'moderator@moderator.com',
+    roles: 'moderator',
+  })
+  return (
+    <Comment
+      comment={{
+        id: 1,
+        name: 'Rob Cameron',
+        body: 'This is the first comment!',
+        createdAt: '2020-01-01T12:34:56Z',
+        postId: 1,
       }}
     />
   )
